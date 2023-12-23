2ea9ad237d169c5b4430d90e30b73a45e91e2959
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2023-12-24 02:50:35 +0300

    refactor: merged PKCE with OAuth

--- oauth/src/commonMain/kotlin/OAuth.kt
+++ oauth/src/commonMain/kotlin/OAuth.kt
@@ -77,6 +77,24 @@ object OAuth {
         // https://datatracker.ietf.org/doc/html/rfc6750#section-3
 
         const val REALM = "realm"
+
+        // https://datatracker.ietf.org/doc/html/rfc7636
+
+        /**
+         * REQUIRED. Code challenge.
+         */
+        const val CODE_CHALLENGE = "code_challenge"
+
+        /**
+         * OPTIONAL, defaults to "plain" if not present in the request.
+         * Code verifier transformation method is "S256" or "plain".
+         */
+        const val CODE_CHALLENGE_METHOD = "code_challenge_method"
+
+        /**
+         * REQUIRED. Code verifier
+         */
+        const val CODE_VERIFIER = "code_verifier"
     }
 
     /**
@@ -204,5 +222,22 @@ object OAuth {
 
         const val BEARER = "bearer"
         const val MAC = "mac"
+    }
+
+    /**
+     * OAuth values registry for parameter `code_challenge_method`
+     */
+    object CodeChallengeMethod {
+        // https://datatracker.ietf.org/doc/html/rfc7636
+
+        /**
+         * code_challenge = code_verifier
+         */
+        const val PLAIN = "plain"
+
+        /**
+         * code_challenge = BASE64URL-ENCODE(SHA256(ASCII(code_verifier)))
+         */
+        const val S256 = "S256"
     }
 }
